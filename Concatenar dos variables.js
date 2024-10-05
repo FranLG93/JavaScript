7e7bdfc83d7215796c435b22e409321613fdebde
FranLG93 <franluengog93@gmail.com>
2024-10-05 13:27:05 +0200

    Rename Concatenar dos variables.js to principiante/Concatenar dos variables.js

--- Concatenar dos variables.js
+++
@@ -1,6 +0,0 @@
-//Concatenacion de dos variables con nombre y apellidos
-
-let nombre = "Francisco"
-let apellido = "Luengo"
-
-console.log("Hola justo me llamo " + nombre + " " + apellido + " y estoy concatenando todo lo que me deja");
---
+++ principiante/Concatenar dos variables.js
@@ -0,0 +1,6 @@
+//Concatenacion de dos variables con nombre y apellidos
+
+let nombre = "Francisco"
+let apellido = "Luengo"
+
+console.log("Hola justo me llamo " + nombre + " " + apellido + " y estoy concatenando todo lo que me deja");
